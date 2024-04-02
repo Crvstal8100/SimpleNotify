0d6bab9ae0b072c7d62c8ccc16ecb176eb2f77c9
Crvstal8100 <99895952+Crvstal8100@users.noreply.github.com>
2024-04-02 21:42:00 +0200

    Create Documentation.lua

---
+++ Documentation.lua
@@ -0,0 +1,5 @@
+local SimpleNotification = loadstring(game:HttpGet('https://raw.githubusercontent.com/Crvstal8100/SimpleNotify/main/UILibrary.lua'))()
+local UI = SimpleNotification:Create("UI_NAME") -- (ui name)
+
+-- HOW TO SEND A NOTIFICATION:
+UI:Notify("TEXT", 5) -- (text, duration)
